b70d37fbf254383d137a05b2d39d7ea53fd9049a
Evilghost3228 <168290167+Evilghost3228@users.noreply.github.com>
2024-04-27 18:51:17 +0300

    Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -8,14 +8,14 @@ public class Calculator {
         String[] inputData = input.split(" ");
 
         if (inputData.length != 3) {
-            throw new IllegalArgumentException("Invalid input");
+            throw new IllegalArgumentException("Неверный Ввод");
         }
 
         int a = Integer.parseInt(inputData[0]);
         int b = Integer.parseInt(inputData[2]);
 
         if (a < 1 || a > 10 || b < 1 || b > 10) {
-            throw new IllegalArgumentException("Numbers should be between 1 and 10");
+            throw new IllegalArgumentException("Числа должны быть от 1 до 10.");
         }
 
         int result;
@@ -31,12 +31,12 @@ public class Calculator {
                 break;
             case "/":
                 if (b == 0) {
-                    throw new IllegalArgumentException("Division by zero is not allowed");
+                    throw new IllegalArgumentException("Деление на ноль не допускается");
                 }
                 result = a / b;
                 break;
             default:
-                throw new IllegalArgumentException("Invalid arithmetic operation");
+                throw new IllegalArgumentException("Неверная арифметическая операция");
         }
 
         System.out.println("Result: " + result);
